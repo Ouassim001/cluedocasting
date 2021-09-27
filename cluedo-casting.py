16656646cc10bc31a23bd2f18b708ee1e57a6a6e
Ouassim001 <89780864+Ouassim001@users.noreply.github.com>
2021-09-27 14:07:10 +0200

    new code

---
+++ cluedo-casting.py
@@ -0,0 +1,15 @@
+vraag1 = input("ben je gezond? ")
+vraag2 = input("heb je acteerlessen gevolgd? ")
+vraag3 = input("ben je een man of een vrouw? ")
+vraag4 = input("hoe zwaar ben je? ")
+vraag5 = input("hoelang ben je? ")
+vraag6 = input("wat voor kleur haar heb je?")
+vraag7 = input("hoe oud ben je? ")
+vraag8 = input("ben je slechtziend? ")
+
+if vraag1 == "ja":
+else:
+    print("je mag geen auditie doen.")
+    if vraag2 == "ja":
+        if vraag3 == "man":
+          print("oke dan kunnen we door met de vragen voor de mannelijke personages.")
