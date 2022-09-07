09f29d8f05cc81a0a3f941514483426cfb6ea6e4
Ouassim001 <89780864+Ouassim001@users.noreply.github.com>
2022-09-07 23:56:03 +0200

    Add files via upload

---
+++ CLUEDO CASTING NEW.py
@@ -0,0 +1,8 @@
+kolonelgeel = input("Je mag op auditie voor de rol van Kolonel van Geelen")
+mvdewit = input("Je mag op auditie voor de rol van Mevrouw de Wit")
+domingroenwo = input("Je mag op auditie voor de rol van Dominee Groenewoud")
+rosarood = input("Je mag op auditie voor de rol van Rosa Roodhart")
+profpimp = input("Je mag op auditie voor de rol van Professor Pimpel")
+mvblauwdraad = input("Je mag op auditie voor de rol van Mevrouw Blaauw van Draet")
+nietgeslaagd = input("Je mag helaas niet op auditie.")
+
---
+++ cluedo-casting.py
@@ -0,0 +1,36 @@
+vraag1 = input("ben je gezond? ")
+vraag2 = input("heb je acteerlessen gevolgd? ")
+vraag3 = input("ben je een man of een vrouw? ")
+vraag4 = input("ben je zwarder dan 70 kilo? ")
+vraag5 = input("ben je langer dan 170 cm? ")
+vraag6 = input("heb je donkergekleurd haar of lichtgekleurd haar?")
+vraag7 = input("ben je ouder dan 30? ")
+vraag8 = input("ben je slechtziend? ")
+
+if vraag1 == "ja":
+    if vraag2 == "ja":
+        if vraag3 == "man":
+            if vraag4 == "ja":
+                if vraag5 == "ja":
+                    if vraag6 == "donkergekleurd":
+                        if vraag7 == "ja":
+                            if vraag8 == "ja":
+                                print("je kan meedoen")
+    else:
+        print("")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+else:
+    print("je mag helaas niet op auditie.")
